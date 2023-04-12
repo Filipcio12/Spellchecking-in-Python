3709f623eb47985835ffd454a89e66188faf4d79
Filipcio12 <121309536+Filipcio12@users.noreply.github.com>
2023-04-12 20:23:38 +0200

    Add files via upload

---
+++ lab4/main.py
@@ -0,0 +1,15 @@
+import distance as ds
+
+
+def main():
+    word1 = "car"
+    word2 = "cat"
+    print(f"levensthein:        {ds.levensthein(word1, word2)}")
+    print(f"hamming:            {ds.hamming(word1, word2)}")
+    print(f"indel:              {ds.indel(word1, word2)}")
+    print(f"levensthein_mod:    {ds.levensthein_mod(word1, word2)}")
+    print(f"hamming_mod:        {ds.hamming_mod(word1, word2)}")
+
+
+if __name__ == "__main__":
+    main()
