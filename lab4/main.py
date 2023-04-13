8847ad0cd2b4f7906544a5d44589080831b7a222
Filipcio12 <121309536+Filipcio12@users.noreply.github.com>
2023-04-13 20:40:37 +0200

    Add files via upload

--- lab4/main.py
+++ lab4/main.py
@@ -1,8 +1,21 @@
 import distance as ds
+from tqdm import tqdm
 
 
-def suggest_word(word):
-    pass
+def suggest_word(misspell, dictionary):
+    if misspell in dictionary:
+        return misspell
+
+    best_word = ""
+    best_distance = float('inf')
+
+    for word in dictionary:
+        distance = ds.levensthein_mod(misspell, word)
+        if distance < best_distance:
+            best_word = word
+            best_distance = distance
+
+    return best_word
 
 
 def main():
@@ -13,6 +26,40 @@ def main():
     print(f"indel:              {ds.indel(word1, word2)}")
     print(f"levensthein_mod:    {ds.levensthein_mod(word1, word2)}")
     print(f"hamming_mod:        {ds.hamming_mod(word1, word2)}")
+    print()
+
+    file = open("words_alpha.txt", "r")
+    dictionary = file.readlines()
+    file.close()
+    for i in range(len(dictionary)):
+        dictionary[i] = dictionary[i][:-1]
+
+    file = open("misspelled.txt", "r")
+    text = file.readlines()
+    file.close()
+
+    file = open("corrected.txt", "w")
+
+    word, new_text = "", ""
+
+    for line in text:
+        word = ""
+        for c in line:
+            if c == ' ' or c == '\n':
+                if len(word) > 0:
+                    word = suggest_word(word, dictionary)
+                    new_text += word + c
+                    word = ""
+                else:
+                    new_text += c
+            else:
+                word += c
+
+    word = suggest_word(word, dictionary)
+    new_text += word
+
+    file.write(new_text)
+    file.close()
 
 
 if __name__ == "__main__":
